377f4e18f7b54777b906e8d97349169cdc3c2db5
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2022-09-03 16:26:24 -0400

    initial commit - homepage in progress

---
+++ script.js
@@ -0,0 +1,16 @@
+function audioPlay() {
+    document.getElementById("player").play();
+};
+
+function audioPause() {
+    document.getElementById("player").pause();
+};
+
+function audioVolumePlus() {
+    document.getElementById("player").volume += 0.1;
+};
+
+function audioVolumeMinus() {
+    document.getElementById("player").volume -= 0.1;
+};
+
