37141b2796596e949296b2bda8596bdea14205ab
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2022-09-05 19:39:27 -0400

    continuing to finish teh homepage

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 I hope you enjoy my website! I will be updating it periodically and adding things as I think of them.
 ### Goals:
 * Build a personal website from scratch which showcases both my software development *and* music portfolios in an organized and pleasing manner
-* Design my own custom audio player using JavaScript and CSS to showcase both my coding skills and my music skills.
+* Design my own custom audio player using JavaScript and CSS to showcase both my coding skills and my music skills, inluding a progress bar.
 * Design a backend with Python to store data for a newsletter.
 * Implement *some* form of fun API just to get used to the process - *yet to be determined*
-* 
 
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//Variable Declarations
+let audio = document.getElementById('player');
+let progress = document.getElementById('progBar');
+let muted = false;
+let composer = true;
+
+//Audio playback functions
+
 function audioPlay() {
     document.getElementById("player").play();
 };
@@ -8,9 +16,47 @@ function audioPause() {
 
 function audioVolumePlus() {
     document.getElementById("player").volume += 0.1;
+    document.getElementById("player").volume = 1;
+    document.getElementById("muteIcon").style.display = "none";
+    document.getElementById("soundIcon").style.display = "";
+    muted = false;
 };
 
 function audioVolumeMinus() {
     document.getElementById("player").volume -= 0.1;
 };
+
+function audioMute() {
+    if (muted) {
+        document.getElementById("player").volume = 1;
+        document.getElementById("muteIcon").style.display = "none";
+        document.getElementById("soundIcon").style.display = "";
+        muted = false;
+    } else {
+        document.getElementById("player").volume = 0;
+        document.getElementById("soundIcon").style.display = "none";
+        document.getElementById("muteIcon").style.display = "";
+        muted = true;
+    }
+};
+
+//progressbar functions
+
+audio.addEventListener('timeupdate', function(event) {
+    progress.style.width = ((audio.currentTime / audio.duration) * 100) + '%';
+    console.log(progress.style.width)
+})
+
+// header button function
+
+function careerToggle() {
+    if (composer) {
+        document.getElementById("composer").innerText = "Composer";
+        composer = false;
+    } else {
+        document.getElementById("composer").innerText = "Developer";
+        composer = true;
+    }
+};
+
 
